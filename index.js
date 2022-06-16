f62e02f1f657aba00603f81f00df26d35b886831
yurigurlie207 <adrienne.yao@gmail.com>
2022-06-16 10:30:10 -0400

    created ingredient and recipe objects

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 // Code your solution here!
 
 document.write("Testing");
+
+let ingredients = {
+    egg: 4,
+    sausages: 2,
+    bread: 5,
+    coffee: 2
+}
+
+const recipes = {
+    eggsandwich: {
+        egg: 2,
+        bread: 2
+    },
+    breakfast: {
+        eggsandwich: 1,
+        coffee: 1
+    }
+}
+
